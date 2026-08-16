aacc294668d39ee21eba04b7ae6786cb69c476ad
Nayad-app <tsnd976@gmail.com>
2026-08-16 20:23:24 +0800

    Load Facebook disable patch and refresh cache v16

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = "nayad-v15";
+const CACHE = "nayad-v16";
 
-const ASSETS = ["./","./manifest.webmanifest","./icon-180.png","./icon-192.png","./icon-512.png","./oauth-fix.js","./render.js","./user-scope.js","./share.js","./invoice-cloud.js","./supplier-cloud.js","./company-label.js"];
+const ASSETS = ["./","./manifest.webmanifest","./icon-180.png","./icon-192.png","./icon-512.png","./oauth-fix.js","./render.js","./user-scope.js","./share.js","./invoice-cloud.js","./supplier-cloud.js","./company-label.js","./facebook-disable.js"];
 
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())); });
@@ -18,6 +18,7 @@ self.addEventListener("fetch", event => {
         if(!patched.includes("invoice-cloud.js")) patched=patched.replace(/<\/body>/i,'<script src="./invoice-cloud.js"></script></body>');
         if(!patched.includes("supplier-cloud.js")) patched=patched.replace(/<\/body>/i,'<script src="./supplier-cloud.js"></script></body>');
         if(!patched.includes("company-label.js")) patched=patched.replace(/<\/body>/i,'<script src="./company-label.js"></script></body>');
+        if(!patched.includes("facebook-disable.js")) patched=patched.replace(/<\/body>/i,'<script src="./facebook-disable.js"></script></body>');
 
         const mobileInvoiceFix=`<style>
 .imageList{touch-action:pan-y;-webkit-user-select:none;user-select:none}
